2013f00b85599d5eaf4f3acfb2703c7398f4c514
paulq1984 <paulquinnell@gmail.com>
2024-04-26 14:28:00 -0400

    added colors

--- OverSteerGP/TeamView.swift
+++ OverSteerGP/TeamView.swift
@@ -12,7 +12,24 @@ struct TeamView: View {
     @Environment(\.modelContext) var modelContext
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(
+                    LinearGradient(gradient: Gradient(colors: [Color("02192B"), Color("#2173B8")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+                .frame(height: geometry.size.height / 2)
+                .overlay(
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .fill(Color.white.opacity(0.2))
+                        .frame(width: .infinity, height: 100)
+                        .overlay(
+                            Text("Team Name").bold()
+                                .font(.largeTitle)
+                        )
+                        .padding()
+                )
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
